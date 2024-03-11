3193bbfa969686beefc851df2fe39f5d3b1445b7
contigen <69436315+contigen@users.noreply.github.com>
2024-03-11 22:37:04 +0100

    Init

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Font Variants. Typography has secrets too.</h1>
+      <p>
+        View font internals and enable font features you never knew existed.
+      </p>
+    </div>
+  )
+}
+
+export default App

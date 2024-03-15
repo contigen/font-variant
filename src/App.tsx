74f34a2e11f7245c32746e4482787eb78adfb23e
contigen <69436315+contigen@users.noreply.github.com>
2024-03-15 08:23:26 +0100

    Modify  text

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,16 @@ import './App.css'
 function App() {
   return (
     <div>
-      <h1 className='text-balance'>
+      <h1>
         Font{' '}
         <span className='bg-gradient-to-br from-[#313b1b] from-50% to-[#1A1A1A] bg-clip-text text-transparent'>
           Variants
         </span>
         .&nbsp;
-        <span className='bg-gradient-to-br from-[#959693] from-50% to-[#1A1A1A] bg-clip-text text-transparent'>
-          Typography
+        <span className='bg-gradient-to-br from-[#959693] from-50% to-[#1A1A1A] bg-clip-text text-transparent pr-[1px]'>
+          Typefaces
         </span>
-        &nbsp;has secrets too.
+        &nbsp;have secrets too.
       </h1>
       <p>
         View font internals and enable font features you never knew existed.
